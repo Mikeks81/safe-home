1951bcc80fcb033f4e3e8647ba98229aadd2378a
Michael Simonitsch <msimonitsch@tammantech.com>
2019-07-07 13:00:53 -0400

    1 st draft of trips data type

--- server/db_config.js
+++ server/db_config.js
@@ -72,6 +72,28 @@ const createUserTable = () => {
     });
 }
 
+
+// @NOTE for the coordinates, do more research on how to store lat and long
+// originally i was going to store each geo point in it's own table "Coordinates" so
+// i could attach all sorts of data to that point. 
+// Still debating on wether i should store it as an array of points OR POSTGIS geography points in
+// the trips table or in it's own table -- i'm leaning towards it's own table with a foreign key to trips
+const createTripsTable = () => {
+  const queryText = 
+    `CREATE TABLE IF NOT EXISTS
+      trips(
+        id SERIAL PRIMARY KEY,
+        start TIMESTAMP,
+        end TIMESTAMP,
+        name TEXT,
+        owner_id INTEGER NOT NULL,
+        coordinates *** NEED TO PICK A DATA TYPE HERE ****
+        created_date TIMESTAMP DEFAULT NOW(),
+        modified_date TIMESTAMP DEFAULT NOW(),
+        FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE
+      )`
+}
+
 /**
  * Drop Contacts Table
  */
