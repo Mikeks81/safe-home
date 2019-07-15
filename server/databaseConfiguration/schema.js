e60ef6a358ac903463e581eba66c90737b0de387
Michael <mikeks81@gmail.com>
2019-07-14 21:20:03 -0400

    adds trips and coordinates table

--- server/databaseConfiguration/schema.js
+++ server/databaseConfiguration/schema.js
@@ -1,6 +1,18 @@
 module.exports = {
   users: 
     `CREATE TABLE IF NOT EXISTS
+      users(
+        id SERIAL PRIMARY KEY,
+        fname TEXT NOT NULL,
+        lname TEXT NOT NULL,
+        phone VARCHAR(15) UNIQUE NOT NULL,
+        email VARCHAR(128) UNIQUE NOT NULL,
+        password VARCHAR(128) NOT NULL,
+        created_date TIMESTAMP DEFAULT NOW(),
+        modified_date TIMESTAMP DEFAULT NOW()
+      )`,
+  contacts:
+    `CREATE TABLE IF NOT EXISTS
       contacts(
         id SERIAL PRIMARY KEY,
         fname TEXT NOT NULL,
@@ -12,16 +24,28 @@ module.exports = {
         modified_date TIMESTAMP DEFAULT NOW(),
         FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE
       )`,
-  contacts:
+  trips:
     `CREATE TABLE IF NOT EXISTS
-      users(
+      trips(
         id SERIAL PRIMARY KEY,
-        fname TEXT NOT NULL,
-        lname TEXT NOT NULL,
-        phone VARCHAR(15) UNIQUE NOT NULL,
-        email VARCHAR(128) UNIQUE NOT NULL,
-        password VARCHAR(128) NOT NULL,
+        start TIMESTAMP,
+        finished TIMESTAMP,
+        name TEXT,
+        owner_id INTEGER NOT NULL,
         created_date TIMESTAMP DEFAULT NOW(),
-        modified_date TIMESTAMP DEFAULT NOW()
-      )`
+        modified_date TIMESTAMP DEFAULT NOW(),
+        FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE
+      )`,
+  coordinates:
+      `CREATE TABLE IF NOT EXISTS
+        coordinates(
+          id SERIAL PRIMARY KEY,
+          time TIMESTAMP,
+          lat FLOAT,
+          long FLOAT,
+          trip_id INTEGER NOT NULL,
+          created_date TIMESTAMP DEFAULT NOW(),
+          modified_date TIMESTAMP DEFAULT NOW(),
+          FOREIGN KEY (trip_id) REFERENCES coordinates (id) ON DELETE CASCADE
+        )`
 }
